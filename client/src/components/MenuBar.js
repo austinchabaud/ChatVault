f8213e5936af25e6038bd3e0ac40ac86d4f0759c
Austin Chabaud <austinc1023@yahoo.com>
2021-08-18 03:30:08 -0500

    rendered posts

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 function MenuBar() {
-	const [activeItem, setActiveItem] = useState('home');
+	const pathname = window.location.pathname;
+
+	const path = pathname === '/' ? 'home' : pathname.substr(1);
+	const [activeItem, setActiveItem] = useState(path);
 
 	const handleItemClick = (e, { name }) => setActiveItem(name);
 
